fb8e3f1bcbbdec6abceb9eb0757ecad76c851668
Andrei Lavreniyuk <andy.lavr@gmail.com>
2018-02-12 11:37:55 +0200

    arch/x86/include/asm: Fix include header

--- arch/x86/include/asm/apm.h
+++ arch/x86/include/asm/apm.h
@@ -7,7 +7,7 @@
 #ifndef _ASM_X86_MACH_DEFAULT_APM_H
 #define _ASM_X86_MACH_DEFAULT_APM_H
 
-#include <asm/spec_ctrl.h>
+#include <asm/nospec-branch.h>
 
 #ifdef APM_ZERO_SEGS
 #	define APM_DO_ZERO_SEGS \
