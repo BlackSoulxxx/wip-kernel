01e1d052d911bdcfad2de093f82f9dd34df07ab7
David Woodhouse <dwmw@amazon.co.uk>
2018-02-14 08:15:21 +0200

    Revert "x86/retpoline: Simplify vmexit_fill_RSB()"
     By putting the RSB filling out of line and calling it, we waste one RSB
    slot for returning from the function itself, which means one fewer
    actual function call we can make if we're doing the Skylake abomination
    of call-depth counting.
    
    It also changed the number of RSB stuffings we do on vmexit from 32,
    which was correct, to 16. Let's just stop with the bikeshedding; it
    didn't actually *fix* anything anyway.
    
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>

--- arch/x86/include/asm/asm-prototypes.h
+++ arch/x86/include/asm/asm-prototypes.h
@@ -38,8 +38,4 @@ INDIRECT_THUNK(dx)
 INDIRECT_THUNK(si)
 INDIRECT_THUNK(di)
 INDIRECT_THUNK(bp)
-asmlinkage void __x86_indirect_thunk(void);
-asmlinkage void __fill_rsb(void);
-asmlinkage void __clear_rsb(void);
-
 #endif /* CONFIG_RETPOLINE */
--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -213,7 +213,6 @@
 #define X86_FEATURE_USE_IBPB		( 7*32+21) /* "" Indirect Branch Prediction Barrier enabled */
 #define X86_FEATURE_USE_IBRS_FW		( 7*32+22) /* "" Use IBRS during runtime firmware calls */
 
-
 /* Virtualization flags: Linux defined, word 8 */
 #define X86_FEATURE_TPR_SHADOW		( 8*32+ 0) /* Intel TPR Shadow */
 #define X86_FEATURE_VNMI		( 8*32+ 1) /* Intel Virtual NMI */
