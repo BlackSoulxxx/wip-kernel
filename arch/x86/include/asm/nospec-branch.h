bf607cd6e6cdb3bb986b1ec88bc3cec0506c249f
Tim Chen <tim.c.chen@linux.intel.com>
2018-02-14 08:27:50 +0200

    x86/firmware: Prevent IBRS from being turned off prematurely.
        Dave Woodhoue proposed to use IBRS to protect the firmware
        call path against Spectre exploit.  However, firmware path
        can go through NMI and we can get nested calls, causing
        unsafe firmware calls with missing IBRS as illustrated below:
    
        firmware_restrict_branch_speculation_start (set IBRS=1)
            NMI
                firmware_restrict_branch_speculation_start (set IBRS=1)
                firmware call
                firmware_restrict_branch_speculation_end (set IBRS=0)
            NMI return
        firmware call (with IBRS=0)  <---- unsafe call, premature IBRS
    disabling
        firmware_restrict_branch_speculation_end (set IBRS=0)
    
        This patch proposes using a per cpu counter to track the IBRS
        firmware call nesting depth, to ensure that we don't turn off
        IBRS prematurely before calling firmware.
    
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>

--- arch/x86/include/asm/nospec-branch.h
+++ arch/x86/include/asm/nospec-branch.h
@@ -205,6 +205,8 @@ static inline bool spectre_v2_ibrs_all(void)
 extern char __indirect_thunk_start[];
 extern char __indirect_thunk_end[];
 
+DECLARE_PER_CPU(int, spec_ctrl_ibrs_fw_depth);
+
 /*
  * On VMEXIT we must ensure that no RSB predictions learned in the guest
  * can be followed in the host, by overwriting the RSB completely. Both
@@ -265,14 +267,16 @@ static inline void firmware_restrict_branch_speculation_end(void)
  */
 static inline void firmware_restrict_branch_speculation_start(void)
 {
-	alternative_msr_write(MSR_IA32_SPEC_CTRL, SPEC_CTRL_IBRS,
+	if (this_cpu_inc_return(spec_ctrl_ibrs_fw_depth) == 1)
+		alternative_msr_write(MSR_IA32_SPEC_CTRL, SPEC_CTRL_IBRS,
 			      X86_FEATURE_USE_IBRS_FW);
 }
 
 static inline void firmware_restrict_branch_speculation_end(void)
 {
-	alternative_msr_write(MSR_IA32_SPEC_CTRL, 0,
-			      X86_FEATURE_USE_IBRS_FW);
+	if (this_cpu_dec_return(spec_ctrl_ibrs_fw_depth) == 0)
+		alternative_msr_write(MSR_IA32_SPEC_CTRL, 0,
+				      X86_FEATURE_USE_IBRS_FW);
 }
 
 /*
